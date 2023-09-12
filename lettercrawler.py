9701aa59bc839abbd1eeba6b47c2c94a843a035b
afoshiok <89757138+afoshiok@users.noreply.github.com>
2023-09-12 16:59:29 -0500

    Update lettercrawler.py

--- lettercrawler.py
+++ lettercrawler.py
@@ -1,11 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 def crawl(username):
     link = f"https://letterboxd.com/{username}/films/diary/"
     page = requests.get(link)
-    soup = BeautifulSoup(page.text, 'html.parser') #Wraps the http request in BS4
-    print(soup.prettify()) #Makes the page HMTL readable 
+    soup = BeautifulSoup(page.content, 'html.parser').tbody #Wraps the http request in BS4
+    data = {
+        "Film Name" : [],
+        "ID": [],
+        "Release Year": []
+    }
+    for i in soup.find_all("td", {"class": "td-film-details"}):
+        data["Film Name"].append(i.h3.text)
+        data["ID"].append(i.div["data-film-id"])
+        data["Release Year"].append(i.span.text)
+
+    film_df = pd.DataFrame(data)
+    
+    print(film_df)
+    
+
+
 
 if __name__ == "__main__":
     crawl('FavourOshio')
