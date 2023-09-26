dc7f7b9fb662a6373a96e1317c5b06f12378f5d2
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-09-26 05:04:58 -0500

    Update lettercrawler.py

--- lettercrawler.py
+++ lettercrawler.py
@@ -5,7 +5,7 @@ def crawl(username):
     link = f"https://letterboxd.com/{username}/films/diary/"
     page = requests.get(link)
     soup = BeautifulSoup(page.text, 'html.parser') #Wraps the http request in BS4
-    print(soup.prettify()) #Makes the page HMTL readable 
+    print(soup.table.prettify()) #Makes the page HMTL readable 
 
 if __name__ == "__main__":
     crawl('FavourOshio')
