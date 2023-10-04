cb0c31f1138cebcc77c7c133a55fbec2e17b8006
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-10-04 14:03:50 -0500

    Still working on it!

--- lettercrawler.py
+++ lettercrawler.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import os
+
+def film_details(id):
+    req_link = f"https://api.themoviedb.org/3/movie/{id}"
+    head = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.environ.get('TOKEN')}" 
+        }
+    
+    response = requests.get(req_link, headers=head)
+    print(response.text)
+
+
 
 def crawl(username):
     link = f"https://letterboxd.com/{username}/films/diary/"
@@ -11,10 +24,12 @@ def crawl(username):
         "ID": [],
         "Release Year": []
     }
-    for i in soup.find_all("td", {"class": "td-film-details"}):
-        data["Film Name"].append(i.h3.text)
-        data["ID"].append(i.div["data-film-id"])
-        data["Release Year"].append(i.span.text)
+    for i in soup.find_all("tr"):
+        film_id = i.find("td", {"class": "td-film-details"}).div["data-film-id"]
+
+        data["Film Name"].append(i.find("h3", {"class": "headline-3 prettify"}).text)
+        data["ID"].append(film_id)
+        data["Release Year"].append("N/A")
 
     film_df = pd.DataFrame(data)
     
@@ -24,4 +39,5 @@ def crawl(username):
 
 
 if __name__ == "__main__":
-    crawl('FavourOshio')
+    # crawl('FavourOshio')
+    film_details(27256)
