f30800498f7fd12e7e03258decefb4363d510ac3
afoshiok <89757138+afoshiok@users.noreply.github.com>
2023-11-09 14:54:31 -0600

    Update lettercrawler.py

--- lettercrawler.py
+++ lettercrawler.py
@@ -29,48 +29,49 @@ async def fetch_film_details(session, id):
     
 
 
-async def crawl(username): #Creates dataframe for data analysis
+async def crawl(username,page): #Creates dataframe for data analysis
     user_films = []
 
     async with aiohttp.ClientSession() as session:
-        link = f"https://letterboxd.com/{username}/films/diary/"
+        link = f"https://letterboxd.com/{username}/films/diary/page/{page}"
         async with session.get(link) as response:
-            soup = BeautifulSoup(await response.text(), 'lxml') #Wraps the http request in BS4
+            if response.status == 200:
+                soup = BeautifulSoup(await response.text(), 'lxml') #Wraps the http request in BS4
 
-            for i in soup.tbody.find_all("tr"):
-                film_slug = i.find("td", {"class": "td-film-details"}).div["data-film-slug"]
+                for i in soup.tbody.find_all("tr"):
+                    film_slug = i.find("td", {"class": "td-film-details"}).div["data-film-slug"]
 
-                tmdb_id = await fetch_tmdb_id(session, film_slug)
-                tmdb_data = await fetch_film_details(session, tmdb_id)
+                    tmdb_id = await fetch_tmdb_id(session, film_slug)
+                    tmdb_data = await fetch_film_details(session, tmdb_id)
 
-                parse_rating = i.find("td", {"class": "td-rating rating-green"})
-                parse_log_date = i.find("td", {"class": "td-day diary-day center"})
+                    parse_rating = i.find("td", {"class": "td-rating rating-green"})
+                    parse_log_date = i.find("td", {"class": "td-day diary-day center"})
 
-                film_rating = int(parse_rating.find('input').get('value'))
-                film_log_date = parse_log_date.find('a').get('href').split('/')[5:8]
-                film_ob = {
-                    "Name": tmdb_data["title"],
-                    "Letterboxd Rating": film_rating,
-                    "TMDb ID": tmdb_id,
-                    "Log Date": f"{film_log_date[1]}-{film_log_date[2]}-{film_log_date[0]}",
-                    "Release Date": tmdb_data["release_date"],
-                    "Budget": tmdb_data["budget"],
-                    "Production Countries":[country.get("iso_3166_1") for country in tmdb_data["production_countries"] ],
-                    "Genre(s)": [genre.get("name") for genre in tmdb_data["genres"]],
-                    "Runtime (Minutes)": tmdb_data["runtime"]
-                }
-                user_films.append(film_ob)
-                    # break #For testing!
-    
-            film_df = pd.DataFrame.from_records(user_films)
-            return film_df
+                    film_rating = int(parse_rating.find('input').get('value'))
+                    film_log_date = parse_log_date.find('a').get('href').split('/')[5:8]
+                    film_ob = {
+                        "Name": tmdb_data["title"],
+                        "Letterboxd Rating": film_rating,
+                        "TMDb ID": tmdb_id,
+                        "Log Date": f"{film_log_date[1]}-{film_log_date[2]}-{film_log_date[0]}",
+                        "Release Date": tmdb_data["release_date"],
+                        "Budget": tmdb_data["budget"],
+                        "Production Countries":[country.get("iso_3166_1") for country in tmdb_data["production_countries"] ],
+                        "Genre(s)": [genre.get("name") for genre in tmdb_data["genres"]],
+                        "Runtime (Minutes)": tmdb_data["runtime"]
+                    }
+                    user_films.append(film_ob)
+                        # break #For testing!
+        
+                film_df = pd.DataFrame.from_records(user_films)
+                return film_df
 
 
 
 if __name__ == "__main__":
     start_time = time.time()
     loop = asyncio.get_event_loop()
-    final_df = loop.run_until_complete(crawl('FavourOshio'))
+    final_df = loop.run_until_complete(crawl('Favourshio',2))
     print(final_df)
     # print(film_details(27256))
     print("--- %s seconds ---" % (time.time() - start_time)) #task runtime
