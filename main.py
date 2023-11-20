e23bbf7531666c392e8ab531264156eaf5681325
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-11-19 18:49:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -36,19 +36,20 @@ if submitted:
 
 if df is not None:
 
-    column_1, column_2 = st.columns(2)
+    
 
-    with column_1:
+    with st.container():
         with tab1:
-            log_years = df.with_columns(pl.col("Log Date").dt.year().cast(pl.Utf8).to_physical().alias("Log_Year"))
+            st.text("Trying to figure out why this isn't working.")
+            log_years = df.with_columns(pl.col("Log Date").dt.year().cast(pl.Utf8).to_physical().alias("Log Year"))
             log_years_script = (
                 log_years.lazy()
-                .group_by("Log_Year")
+                .group_by("Log Year")
                 .agg(pl.count("Log Date").alias("Films Logged"))
-                .sort("Log_Year")
+                .sort("Log Year")
             )
             log_years_count = log_years_script.collect()
-            log_years_graph = px.bar(log_years_count, x="Log_Year", y="Films Logged", title="Films Logged by Year")
+            log_years_graph = px.bar(log_years_count, x="Log Year", y="Films Logged", title="Films Logged by Year")
             log_years_graph.update_xaxes(type='category')
             st.plotly_chart(log_years_graph, use_container_width=True)
         
@@ -62,51 +63,57 @@ if df is not None:
                 .sort("Release Year")
             )
             re_count = release_years_script.collect()
-            re_years_graph = px.bar(re_count, x="Release Year", y="count")
+            re_years_graph = px.bar(re_count, x="Release Year", y="count", title="Films Logged by Release Year")
             re_years_graph.update_xaxes(type='category')
             st.plotly_chart(re_years_graph, use_container_width=True)
 
-
-    add_vertical_space(2)
+    st.divider()
+                
     
+    with st.container():
+        pie_color = ['#ff8000', '#00e054', '#ffffff', '#40bcf4' ]
+        st.header("Writer/Director Demographics")
+        column_1, column_2 = st.columns(2)
+        with column_1:
+            director_gender = df.select(
+                                pl.col("Director Gender").list.count_matches(2).alias("Male Directors"),
+                                pl.col("Director Gender").list.count_matches(1).alias("Female Directors"),
+                                pl.col("Director Gender").list.count_matches(3).alias("Non-Binary Directors"),
+                                pl.col("Director Gender").list.count_matches(0).alias("Not Specified")
+                                )
+            director_gender_sum = director_gender.select(
+                pl.col("Male Directors").sum(),
+                pl.col("Female Directors").sum(),
+                pl.col("Non-Binary Directors").sum(),
+                pl.col("Not Specified").sum()
+            ).transpose(include_header=True,header_name="categories")
+            st.subheader("Director Demographics")
+            director_donut = go.Figure(data=[go.Pie(labels=director_gender_sum.to_series(0), values=director_gender_sum.to_series(1), hole=.4, marker=dict(colors=pie_color))])
+            st.plotly_chart(director_donut)
+        with column_2:
+            writer_gender = df.select( 
+                                    pl.col("Writer Gender").list.count_matches(2).alias("Male Writers"),
+                                    pl.col("Writer Gender").list.count_matches(1).alias("Female Writers"),
+                                    pl.col("Writer Gender").list.count_matches(3).alias("Non-Binary Writers"),
+                                    pl.col("Writer Gender").list.count_matches(0).alias("Not Specified")
+                                    )
+            writer_gender_sum = writer_gender.select(
+                pl.col("Male Writers").sum(),
+                pl.col("Female Writers").sum(),
+                pl.col("Non-Binary Writers").sum(),
+                pl.col("Not Specified").sum()
+            ).transpose(include_header=True,header_name="categories")
+            st.subheader("Writer Demographics")
+            writer_donut = go.Figure(data=[go.Pie(labels=writer_gender_sum.to_series(0), values=writer_gender_sum.to_series(1), hole=.4, marker=dict(colors=pie_color))])
+            st.plotly_chart(writer_donut)
 
-    director_gender = df.select(
-                        pl.col("Director Gender").list.count_matches(2).alias("Male Directors"),
-                        pl.col("Director Gender").list.count_matches(1).alias("Female Directors"),
-                        pl.col("Director Gender").list.count_matches(3).alias("Non-Binary Directors"),
-                        pl.col("Director Gender").list.count_matches(0).alias("Not Specified")
-                        )
-    director_gender_sum = director_gender.select(
-        pl.col("Male Directors").sum(),
-        pl.col("Female Directors").sum(),
-        pl.col("Non-Binary Directors").sum(),
-        pl.col("Not Specified").sum()
-    ).transpose(include_header=True,header_name="categories")
-
-    writer_gender = df.select( 
-                            pl.col("Writer Gender").list.count_matches(2).alias("Male Writers"),
-                            pl.col("Writer Gender").list.count_matches(1).alias("Female Writers"),
-                            pl.col("Writer Gender").list.count_matches(3).alias("Non-Binary Writers"),
-                            pl.col("Writer Gender").list.count_matches(0).alias("Not Specified")
-                            )
-    writer_gender_sum = writer_gender.select(
-        pl.col("Male Writers").sum(),
-        pl.col("Female Writers").sum(),
-        pl.col("Non-Binary Writers").sum(),
-        pl.col("Not Specified").sum()
-    ).transpose(include_header=True,header_name="categories")
-
-    director_donut = go.Figure(data=[go.Pie(labels=director_gender_sum.to_series(0), values=director_gender_sum.to_series(1), hole=.3)])
-    writer_donut = go.Figure(data=[go.Pie(labels=writer_gender_sum.to_series(0), values=writer_gender_sum.to_series(1), hole=.3)])
-
-    
-    
-    with column_1:
-        st.subheader("Writer Demographics")
-        st.plotly_chart(writer_donut, use_container_width=True)
-    with column_2:
-        st.subheader("Director Demographics")
-        st.plotly_chart(director_donut, use_container_width=True)
+        
+        
+        
+        
+            
+        
+            
 
     
 
