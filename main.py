2d82a18f22e8805f0786c62fe624b43b05065f64
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-12-06 14:03:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def load_user_data(username):
         st.error(f"Error loading user data: {str(e)}")
         return None
 
-username = st.text_input('Username')
+username = st.text_input('Username', placeholder="Tip: To avoid errors, don't copy + paste the examples.")
 st.markdown("""_If you don't have a Letterboxd try some of my favorite accounts: :green[fumilayo] (Ayo Edebiri), :green[jaredgilman] (Jared Gilman), :green[girlactress] (Rachel Sennott)_ """)
 submitted = st.button("Submit")
 df = None
